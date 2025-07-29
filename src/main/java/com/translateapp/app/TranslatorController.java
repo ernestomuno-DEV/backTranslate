71715b3a3d1e9842954c780285e088fbde64065b
Ernesto Ramirez <rmunoz.ernesto22@gmail.com>
2025-07-28 22:27:19 -0600

    configuracion de CORS

--- src/main/java/com/translateapp/app/TranslatorController.java
+++ src/main/java/com/translateapp/app/TranslatorController.java
@@ -1,10 +1,8 @@
 package com.translateapp.app;
 
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
+@CrossOrigin(origins = "*")
 @RestController
 @RequestMapping("/api/translate")
 public class TranslatorController {
